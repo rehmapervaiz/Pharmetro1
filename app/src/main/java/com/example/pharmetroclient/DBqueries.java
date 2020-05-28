bbbfd5db4282c83c8f86d24c8d8b3c43ce50807e
rabiya23 <rabiyausman23@gmail.com>
2020-05-28 23:21:34 +0500

    hgkkgiygh

--- app/src/main/java/com/example/pharmetroclient/DBqueries.java
+++ app/src/main/java/com/example/pharmetroclient/DBqueries.java
@@ -8,7 +8,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
+import java.*;
 import java.util.ArrayList;
 import java.util.List;
 
