21ed891f3d544e92e676c421c012939958b5a813
Rehmapervaiz <rehmapervaiz15@gmail.com>
2020-06-02 22:45:00 +0500

    video 56 57
    Files in which Changes performed
    product Description Fragment
    product Details Activity
    product Details Adapter
    Product Specification Fragment
    product spec item layout.XML

--- app/src/main/java/com/example/pharmetroclient/ProductDescriptionFragment.java
+++ app/src/main/java/com/example/pharmetroclient/ProductDescriptionFragment.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
 
 
 /**
@@ -19,12 +21,29 @@ public class ProductDescriptionFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private TextView desciptionBody;
+  //  public static String productDescription;
+
+    public String body;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_description, container, false);
+        View view= inflater.inflate(R.layout.fragment_product_description, container, false);
+        desciptionBody=view.findViewById ( R.id.tv_product_description );
+     //   if(tabPosition==0){
+       //     desciptionBody.setText ( productDescription );
+       // }else{
+        //    desciptionBody.setText ( productOtherDetails );
+        //}
+
+       // desciptionBody.setText ( productDescription );
+
+
+        desciptionBody.setText ( body   );
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/pharmetroclient/ProductSpecificationFragment.java
+++ app/src/main/java/com/example/pharmetroclient/ProductSpecificationFragment.java
@@ -10,8 +10,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 
 /**
@@ -25,23 +25,24 @@ public class ProductSpecificationFragment extends Fragment {
     }
 
     private RecyclerView productSpecificationRecyclerView;
+    public List<ProductSpecificationModel> productSpecificationModelList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view= inflater.inflate(R.layout.fragment_product_specification, container, false);
+        View view = inflater.inflate ( R.layout.fragment_product_specification, container, false );
 
-        productSpecificationRecyclerView =  view.findViewById(R.id.product_specification_recyclerview);
+        productSpecificationRecyclerView = view.findViewById ( R.id.product_specification_recyclerview );
 
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(view.getContext());
-        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager ( view.getContext () );
+        linearLayoutManager.setOrientation ( RecyclerView.VERTICAL );
 
-        productSpecificationRecyclerView.setLayoutManager(linearLayoutManager);
+        productSpecificationRecyclerView.setLayoutManager ( linearLayoutManager );
 
-        List<ProductSpecificationModel>productSpecificationModelList=new ArrayList<>();
-        productSpecificationModelList.add(new ProductSpecificationModel(0,"General"));
+      //  productSpecificationModelList =new ArrayList<> ();
+       /* productSpecificationModelList.add(new ProductSpecificationModel(0,"General"));
         productSpecificationModelList.add(new ProductSpecificationModel(1,"Dose","500mg"));
         productSpecificationModelList.add(new ProductSpecificationModel(1,"Dose","500mg"));
         productSpecificationModelList.add(new ProductSpecificationModel(1,"Dose","500mg"));
@@ -54,10 +55,12 @@ public class ProductSpecificationFragment extends Fragment {
         productSpecificationModelList.add(new ProductSpecificationModel(1,"Dose","500mg"));
         productSpecificationModelList.add(new ProductSpecificationModel(1,"Dose","500mg"));
         productSpecificationModelList.add(new ProductSpecificationModel(1,"Dose","500mg"));
+*/
 
-        ProductSpecificationAdapter productSpecificationAdapter=new ProductSpecificationAdapter(productSpecificationModelList);
-        productSpecificationRecyclerView.setAdapter(productSpecificationAdapter);
-        productSpecificationAdapter.notifyDataSetChanged();
+
+        ProductSpecificationAdapter productSpecificationAdapter = new ProductSpecificationAdapter (productSpecificationModelList );
+        productSpecificationRecyclerView.setAdapter ( productSpecificationAdapter );
+        productSpecificationAdapter.notifyDataSetChanged ();
         return view;
     }
 
